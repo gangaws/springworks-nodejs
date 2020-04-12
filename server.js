aaf4968def9c4fd4e96611ef104c8a7e9bf7f087
Gangaiah Gaddam <gangaiah.gaddam@neobric.com>
2020-04-12 18:06:36 +0530

    reponse modified

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World, Springworks\n')
+  response.end('Hello, Springworks.....\n')
 })
 
 server.listen(port)
